d43650e548a9bd5f3340193d50fad00303a6bcee
Roman <gambro87@gmail.com>
2017-07-31 17:04:34 +0300

    fix constant and space

--- lib/ace_of_price.rb
+++ lib/ace_of_price.rb
@@ -4,7 +4,7 @@ require "net/http"
 class AceOfPrice
   BASE_URL = 'http://localhost:3000/'
   SUBSCRIBE_PATH = 'api/subscriptions/'
-  UNSUBSCRIPBE_PATH = 'api/subscriptions/delete'
+  UNSUBSCRIBE_PATH = 'api/subscriptions/delete'
 
   def self.subscribe(key: nil, id: nil, url: nil)
     base_url = BASE_URL.concat(SUBSCRIBE_PATH)
@@ -12,7 +12,7 @@ class AceOfPrice
   end
 
   def self.unsubscribe(key: nil, url: nil)
-    base_url = BASE_URL.concat(UNSUBSCRIPBE_PATH)
+    base_url = BASE_URL.concat(UNSUBSCRIBE_PATH)
     send_request(base_url, key: key, url: url) if key && url
   end
 
@@ -21,9 +21,9 @@ private
 
   def send_request(base_url, key: nil, url: nil, id: nil)
     params = {
-      'token' => key,
-      'external_id'  => id,
-      'url' => url
+      'token'         => key,
+      'external_id'   => id,
+      'url'           => url
     }
 
     Net::HTTP.post_form(URI.parse(base_url), params)
