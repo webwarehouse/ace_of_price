bc016a64b60e37b167c02082a50a2df9c1970fda
Roman <gambro87@gmail.com>
2017-07-31 19:26:21 +0300

    fix class methods

--- lib/ace_of_price.rb
+++ lib/ace_of_price.rb
@@ -6,26 +6,27 @@ class AceOfPrice
   SUBSCRIBE_PATH = 'api/subscriptions/'
   UNSUBSCRIBE_PATH = 'api/subscriptions/delete'
 
-  def self.subscribe(key: nil, id: nil, url: nil)
-    base_url = BASE_URL.concat(SUBSCRIBE_PATH)
-    send_request(base_url, key: key, id: id, url: url) if key && url && id
-  end
+  class << self
 
-  def self.unsubscribe(key: nil, url: nil)
-    base_url = BASE_URL.concat(UNSUBSCRIBE_PATH)
-    send_request(base_url, key: key, url: url) if key && url
-  end
+    def subscribe(key: nil, id: nil, url: nil)
+      base_url = BASE_URL.concat(SUBSCRIBE_PATH)
+      AceOfPrice.send_request(base_url, key, url, id) if key && url && id
+    end
 
+    def unsubscribe(key: nil, url: nil)
+      base_url = BASE_URL.concat(UNSUBSCRIBE_PATH)
+      AceOfPrice.send_request(base_url, key, url, nil) if key && url
+    end
 
-private
+    def send_request(base_url, key, url, id)
+      params = {
+        'token'         => key,
+        'external_id'   => id,
+        'url'           => url
+      }
 
-  def send_request(base_url, key: nil, url: nil, id: nil)
-    params = {
-      'token'         => key,
-      'external_id'   => id,
-      'url'           => url
-    }
+      Net::HTTP.post_form(URI.parse(base_url), params)
+    end
 
-    Net::HTTP.post_form(URI.parse(base_url), params)
   end
 end
